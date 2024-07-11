87e355392a5086e846d4d876fcb1c0f36f0cb88a
PutraRaimarDelvin <delvinputra805@gmail.com>
2024-07-11 13:15:01 +0700

    13

--- stackusingsinly.cpp
+++ stackusingsinly.cpp
@@ -39,4 +39,6 @@ public:
 		cout << "Popped value: " << top->data << endl;
 		top = top->next; // Update the top pointer to the next node
 	}
-	
+	// Peek/Top operation: Retrieve the value of the topmost element without removing it void peek() {
+	void peek() {
+		
