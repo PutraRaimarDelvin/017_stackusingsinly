1602866f90f62ee3c8e34241277b09d6020e5afb
PutraRaimarDelvin <delvinputra805@gmail.com>
2024-07-11 13:15:45 +0700

    15

--- stackusingsinly.cpp
+++ stackusingsinly.cpp
@@ -54,3 +54,10 @@ public:
 		}// Return the value of the top node
 	}
 
+
+	// IsEmpty operation: Check if the stack is empty
+	bool isEmpty() {
+		return top == NULL; // Return true if the top pointer is NULL, indicating an empty stack
+	}
+};
+
