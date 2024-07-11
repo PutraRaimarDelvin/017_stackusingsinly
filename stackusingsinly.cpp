c87d345a349859ae8efdc0a0e85b29e915e5c161
PutraRaimarDelvin <delvinputra805@gmail.com>
2024-07-11 13:13:12 +0700

    10

--- stackusingsinly.cpp
+++ stackusingsinly.cpp
@@ -26,4 +26,9 @@ public:
 		Node* newNode = new Node(); // 1. Allocate memory for the new node
 		newNode->data = value; // 2. assign value
 		newNode->next = top;// 3. Set the next pointer of the new node to the current top node newNode
-		
+		top = newNode; // 4. Update the top pointer to the new node
+		cout << "push value " << value << endl;
+		return value;
+	}
+
+	
